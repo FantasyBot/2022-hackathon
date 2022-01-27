5679e3a638617712e1ff754fb1294da459756d13
FantasyBot <usernamegithub@gmail.com>
2022-01-27 12:31:51 +0400

    Add KYC functionality

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getKyc } = require("../controllers/sessionController");
+const { getKyc, webHook } = require("../controllers/sessionController");
 const { protect } = require("../middleware/authMiddleware");
 // const { isOperatorActive } = require("../middleware/isOperatorMiddleware");
 
 // /api/session...
 router.post("/", protect, getKyc);
+router.post("/kwb", webHook);
+
 
 module.exports = router;
--- backend/utils/kycApis.js
+++ backend/utils/kycApis.js
@@ -1,31 +1,44 @@
 const axios = require("axios");
 const dotenv = require("dotenv").config();
+const error = require("../libs/error");
 
+// handle kyc session start
 const sendKycRequest = async () => {
-  const token = await axios.post(
+  // get kyc access token
+  const { data } = await axios.post(
     "https://kyc.dev.sweeftdigital.com:8081/client/token",
     {
-      organization: process.env.ORGANISATION,
-      clientName: process.env.CLIENTNAME,
-      key: process.env.KEY,
+      organization: process.env.KYC_ORGANISATION,
+      clientName: process.env.KYC_CLIENTNAME,
+      key: process.env.KYC_KEY,
+    }
+  );
+  const accessToken = data?.access_token;
+
+  // check
+  if (!accessToken) {
+    throw error("KYC_ERROR", 400);
+  }
+
+  /* 
+    request session creation from kyc api
+    requires authorization header
+  */
+  const sessionRes = await axios.post(
+    "https://kyc.dev.sweeftdigital.com:8081/auth/create-session",
+    {
+      redirectUrl: "facebook.com",
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
     }
   );
 
-  return `HERE ${token}`;
-
-  // const verify = await axios.post(
-  //   "https://kyc.dev.sweeftdigital.com:8081/auth/create-session",
-  //   {
-  //     redirectUrl: "facebook.com",
-  //   },
-  //   {
-  //     headers: {
-  //       Authorization: `Bearer ${token.data.access_token}`,
-  //     },
-  //   }
-  // );
-
-  // return verify;
+  const kycSession = sessionRes.data;
+  
+  return kycSession;
 };
 
 module.exports = {
